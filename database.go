87e5e5b80856fb619e9c0682c734097531781f78
tienpvse1 <tienpvse@fpt.edu.vn>
2022-01-08 20:00:25 +0700

    resolved: docker file

--- database.go
+++ database.go
@@ -6,7 +6,7 @@ import (
 )
 
 func InitConnection() *gorm.DB {
-	dsn := "root:soscretthatnearlycannotbefindout@tcp(host.docker.internal:3307)/go_image_uploader"
+	dsn := "root:soscretthatnearlycannotbefindout@tcp(172.17.0.1:3308)/go_image_uploader"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("cannot connect to mysql database")
